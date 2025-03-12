7d21ef3dfda084793246746bc2ec6fe758c78c4e
CLOE ALBERTO DE SOUZA <cloe.alberto@icloud.com>
2025-03-11 21:08:44 -0300

    Initial commit

---
+++ src/main/java/br/com/freemann/Decolatech2025AvanadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.freemann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Decolatech2025AvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Decolatech2025AvanadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/freemann/Decolatech2025AvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.freemann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decolatech2025AvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
